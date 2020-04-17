2bfbc29db971d0849a642c9c682f970c93ff47a6
Jason Douglas <40008798+dougjaso@users.noreply.github.com>
2020-04-17 13:28:19 -0700

    Updated default_intent_fulfillment.py
    Added comments to clarify what can be done here.

--- python/LexDefaultIntentUtterance/default_intent_fulfillment.py
+++ python/LexDefaultIntentUtterance/default_intent_fulfillment.py
@@ -1,5 +1,6 @@
 import json
 def lambda_handler(event, context):
+    
     use_response = {
         "sessionAttributes": {
             "detected_utterance": event['inputTranscript']
@@ -8,7 +9,9 @@ def lambda_handler(event, context):
             "type": "Close",
             "fulfillmentState": "Fulfilled",
             "message": {
+                # Can be PlainText or SSML
                 "contentType": "PlainText",
+                # Edit the content value below with the message you want to return to the customer.
                 "content": "I'm not sure how to hande your request. Let me find an agent to assist."
             }
         }
