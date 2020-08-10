b73aaaeafec56408418be410e739a034ffb945f3
Jason Douglas <40008798+dougjaso@users.noreply.github.com>
2020-08-10 11:11:10 -0700

    Update lambda_function.py

--- python/contactflowhelper/lambda_function.py
+++ python/contactflowhelper/lambda_function.py
@@ -19,84 +19,95 @@ import random
 
 def lambda_handler(event, context):
     response = {}
+    response.update({'result':'success'})
+    operation_requested = event['Details']['Parameters']['operation']
+    operation_parameters = event['Details']['Parameters']
     
-    if event['Details']['Parameters']['function'] == 'freeform_math':
-        response.update({'answer':str(freeform_math(event))})
+    if operation_requested == 'freeform_math':
+        response.update({'answer':str(freeform_math(operation_parameters))})
         
-    elif event['Details']['Parameters']['function'] == 'random_number':
-        response.update({'answer':str(random_number(event))})
+    elif operation_requested == 'random_number':
+        response.update({'answer':str(random_number(operation_parameters))})
         
-    elif event['Details']['Parameters']['function'] == 'increment_1':
-        response.update({'answer':str(increment_1(event))})
+    elif operation_requested == 'increment_1':
+        response.update({'answer':str(increment_1(operation_parameters))})
         
-    elif event['Details']['Parameters']['function'] == 'increment_n':
-        response.update({'answer':str(increment_n(event))})
+    elif operation_requested == 'increment_n':
+        response.update({'answer':str(increment_n(operation_parameters))})
         
-    elif event['Details']['Parameters']['function'] == 'random_choice':
-        response.update({'answer':str(random_choice(event))})
+    elif operation_requested == 'random_choice':
+        response.update({'answer':str(random_choice(operation_parameters))})
         
-    elif event['Details']['Parameters']['function'] == 'replace_text':
-        response.update({'answer':str(replace_text(event))})
+    elif operation_requested == 'replace_text':
+        response.update({'answer':str(replace_text(operation_parameters))})
         
-    elif event['Details']['Parameters']['function'] == 'split_text':
-        response.update(split_text(event))
+    elif operation_requested == 'split_text':
+        response.update(split_text(operation_parameters))
         
-    elif event['Details']['Parameters']['function'] == 'strip_text':
-        response.update({'answer': str(strip_text(event))})
+    elif operation_requested == 'strip_text':
+        response.update({'answer': str(strip_text(operation_parameters))})
         
-    elif event['Details']['Parameters']['function'] == 'upper_text':
-        response.update({'answer': str(upper_text(event))})
+    elif operation_requested == 'upper_text':
+        response.update({'answer': str(upper_text(operation_parameters))})
         
-    elif event['Details']['Parameters']['function'] == 'lower_text':
-        response.update({'answer': str(lower_text(event))})
+    elif operation_requested == 'lower_text':
+        response.update({'answer': str(lower_text(operation_parameters))})
         
     else:
         response.update({'result':'fail'})
         response.update({'reason':'no valid function passed'})
     
-    response.update({'result':'success'})
     return response
     
-def freeform_math(event):
-    result = eval(event['Details']['Parameters']['expression'])
+def freeform_math(operation_parameters):
+    result = eval(operation_parameters['expression'])
     return result
     
-def random_number(event):
-    result = round(random.uniform(int(event['Details']['Parameters']['start']),int(event['Details']['Parameters']['end'])))
+def random_number(operation_parameters):
+    result = round(random.uniform(int(operation_parameters['start']),int(operation_parameters['end'])))
     return result
     
-def increment_1(event):
-    result = int(event['Details']['Parameters']['base'])+1
+def increment_1(operation_parameters):
+    result = int(operation_parameters['base'])+1
     return result
     
-def increment_n(event):
-    result = int(event['Details']['Parameters']['base'])+int(event['Details']['Parameters']['increment'])
+def increment_n(operation_parameters):
+    result = int(operation_parameters['base'])+int(operation_parameters['increment'])
     return result
     
-def random_choice(event):
-    choice_list = event['Details']['Parameters']['list'].split(',')
+def random_choice(operation_parameters):
+    choice_list = operation_parameters['list'].split(',')
     result = random.choice(choice_list)
     return result
     
-def replace_text(event):
-    do_replace = event['Details']['Parameters']['text_string']
-    result = do_replace.replace(event['Details']['Parameters']['replace_this'], event['Details']['Parameters']['with_this'])
+def replace_text(operation_parameters):
+    do_replace = operation_parameters['text_string']
+    result = do_replace.replace(operation_parameters['replace_this'], operation_parameters['with_this'])
     return result
     
-def split_text(event):
-    text_to_split = event['Details']['Parameters']['text_string']
-    split_at = event['Details']['Parameters']['split_at']
-    split_result = text_to_split.split(split_at,1)
-    result = {
-        'segment_1': str(split_result[0]),
-        'segment_2': str(split_result[1])
-        }
+def split_text(operation_parameters):
+    split_values = {}
+    split_counter = 0
+    text_to_split = operation_parameters['text_string']
+    split_at = operation_parameters['split_at']
+    try:
+        split_max = int(operation_parameters['split_max'])
+    except:
+        split_max = -1
+    split_result = text_to_split.split(split_at,split_max)
+    for string in split_result:
+        split_counter = split_counter + 1
+        split_values.update({
+            'segment'+str(split_counter):string
+        })
+    split_values.update({'total_segments':str(split_counter)})
+    result = split_values
     return result
     
-def strip_text(event):
-    text_to_strip = event['Details']['Parameters']['text_string']
-    what_to_strip = event['Details']['Parameters']['strip_this']
-    strip_mode = event['Details']['Parameters']['mode']
+def strip_text(operation_parameters):
+    text_to_strip = operation_parameters['text_string']
+    what_to_strip = operation_parameters['strip_this']
+    strip_mode = operation_parameters['mode']
     if strip_mode == 'trim':
         result = text_to_strip.strip(what_to_strip)
     if strip_mode == 'right':
@@ -105,10 +116,10 @@ def strip_text(event):
         result = text_to_strip.lstrip(what_to_strip)
     return result
         
-def upper_text(event):
-    result = event['Details']['Parameters']['text_string'].upper()
+def upper_text(operation_parameters):
+    result = operation_parameters['text_string'].upper()
     return result
     
-def lower_text(event):
-    result = event['Details']['Parameters']['text_string'].lower()
+def lower_text(operation_parameters):
+    result = operation_parameters['text_string'].lower()
     return result
