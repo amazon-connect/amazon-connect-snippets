237a74b8cbcc30d3d14fbbb715947ca7eff414a5
Gillian Armstrong <gillianq@amazon.co.uk>
2023-06-05 20:49:07 +0000

    Temporary location for Lex examples until move into aws-samples.

---
+++ projects/AmazonLexv2LambdaIntegrationExamples/src/reminderBotLex2Lambda/intentHandlers/constants.ts
@@ -0,0 +1,8 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+export const enum IntentNames {
+  CALL_INTENT = "CallIntent",
+  EMAIL_INTENT = "EmailIntent",
+  FALLBACK_INTENT = "FallbackIntent",
+}
